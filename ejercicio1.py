de5001428e735a02cea6a232b5b88d5af01c0ce8
alejandroruizgtz <55504813+alejandroruizgtz@users.noreply.github.com>
2019-09-18 11:56:28 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,16 @@
+# Crear un progama que despliege su nombre y su direccion de tal manera que pueda escribirse en un sobre de correo. Su programa no tiene que leer ningun dato de entrada por el usuario.
+
+print('Araceli Gutierrez Muñoz')
+print('Mi mama')
+print('Calle Hidalgo 122 C.P. 05000')
+print('Cuajimalpa')
+print('Ciudad de mexico')
+print(' ')
+print(' ')
+print(' ')
+
+print('                           alejandro ruiz')
+print('                           tu hijo fav')
+print('                           Calle Hidalgo 122')
+print('                           Cuajimalpa')
+print('                           Ciudad de mexico')
