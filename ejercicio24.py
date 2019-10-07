07bf8c2daa4f2d1fe53d9db9ce9b8d919ef0608e
alejandroruizgtz <55504813+alejandroruizgtz@users.noreply.github.com>
2019-10-06 23:43:46 -0500

    Add files via upload

---
+++ ejercicio24.py
@@ -0,0 +1,20 @@
+# Ejercicio 24. Unidades de tiempo 1
+#crear un programa que le pida al usuario la duracion en dias,
+#horas, minutos y segundos. Calcualar y desplegar la cantidad
+#total de segundos de duracion.
+
+
+
+dias = float(input('cuantos dias: '))
+horas = float(input('cuantas horas: '))
+min = float(input('cuantos minutos: '))
+seg = float(input('cuantos segundos: '))
+
+c = float(min*60)
+a = float(dias*86400)
+
+b = float(horas*3600)
+
+
+
+print('los segundos en total:', a+b+c+seg)
